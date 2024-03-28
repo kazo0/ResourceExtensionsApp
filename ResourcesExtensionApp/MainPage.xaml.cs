872932dc5f9b5e3c35378db6f32411a56408d608
Steve Bilogan <steve.bilogan@gmail.com>
2024-03-27 22:18:12 -0400

    done

--- ResourcesExtensionApp/MainPage.xaml.cs
+++ ResourcesExtensionApp/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Uno.Toolkit.UI;
+
 namespace ResourcesExtensionApp;
 
 public sealed partial class MainPage : Page
@@ -5,5 +7,22 @@ public sealed partial class MainPage : Page
 	public MainPage()
 	{
 		this.InitializeComponent();
+
+		Loaded += OnLoaded;
+		
+	}
+
+	private void OnLoaded(object sender, RoutedEventArgs e)
+	{
+		ThemeToggle.IsOn = !SystemThemeHelper.IsRootInDarkMode(XamlRoot);
+
+		ThemeToggle.Toggled += ToggleSwitch_Toggled;
+	}
+
+	private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+	{
+		if (sender is not ToggleSwitch toggle) return;
+
+		SystemThemeHelper.SetRootTheme(toggle.XamlRoot, darkMode: !toggle.IsOn);
 	}
 }
